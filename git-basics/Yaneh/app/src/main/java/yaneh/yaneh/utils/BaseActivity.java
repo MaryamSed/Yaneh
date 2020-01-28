d5a01ca0c24a059bcf5603d45a15bf75c5304327
Maryam <Sedaghati85@gmail.com>
2020-01-28 21:37:37 +0330

    add dialog in basicactivity

--- git-basics/Yaneh/app/src/main/java/yaneh/yaneh/utils/BaseActivity.java
+++ git-basics/Yaneh/app/src/main/java/yaneh/yaneh/utils/BaseActivity.java
@@ -1,12 +1,24 @@
 package yaneh.yaneh.utils;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
     public Context mcontext = this;
     public Activity mactivity = this;
+
+    public ProgressDialog dialog;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        dialog = new ProgressDialog(this);
+        dialog.setTitle("loading");
+        dialog.setMessage("Please waitting");
+    }
 
 
 }
