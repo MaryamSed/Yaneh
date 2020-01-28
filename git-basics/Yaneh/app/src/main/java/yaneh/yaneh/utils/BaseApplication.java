f998407c13bc2b577fb0bce6e07febc0b9c9f7e7
Maryam <Sedaghati85@gmail.com>
2020-01-28 21:41:37 +0330

    add Hawk.init in BaseApplication

--- git-basics/Yaneh/app/src/main/java/yaneh/yaneh/utils/BaseApplication.java
+++ git-basics/Yaneh/app/src/main/java/yaneh/yaneh/utils/BaseApplication.java
@@ -3,6 +3,8 @@ package yaneh.yaneh.utils;
 import android.app.Application;
 import android.graphics.Typeface;
 
+import com.orhanobut.hawk.Hawk;
+
 public class BaseApplication extends Application {
     public static BaseApplication app;
     public static Typeface appface;
@@ -12,6 +14,8 @@ public class BaseApplication extends Application {
         super.onCreate();
         app = this;
         appface = Typeface.createFromAsset(getAssets(),"B Yekan+.ttf");
+        Hawk.init(this).build();
+
     }
 
     @Override
@@ -19,3 +23,11 @@ public class BaseApplication extends Application {
         super.onTerminate();
     }
 }
+
+
+//    @Override
+//    public void onCreate() {
+//        super.onCreate();
+//        Hawk.init(this).build();
+//
+//    }
