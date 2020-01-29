815b7ffc40733f69ae2f726b084679eef6df405e
Maryam <Sedaghati85@gmail.com>
2020-01-29 13:19:42 +0330

    create Identity class for name,family,mobile

---
+++ git-basics/Yaneh/app/src/main/java/yaneh/yaneh/Register/Identity.java
@@ -0,0 +1,31 @@
+package yaneh.yaneh.Register;
+
+public class Identity {
+    private String name;
+    private String family;
+    private String mobile;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
