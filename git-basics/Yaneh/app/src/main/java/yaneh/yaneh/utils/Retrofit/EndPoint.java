e1c9690cbbc98012fd6adcfed68c0e6a104471d6
Maryam <Sedaghati85@gmail.com>
2020-01-29 11:26:02 +0330

    add endpoint for using retrofit. Thies codes are inactive

--- git-basics/Yaneh/app/src/main/java/yaneh/yaneh/utils/Retrofit/EndPoint.java
+++ git-basics/Yaneh/app/src/main/java/yaneh/yaneh/utils/Retrofit/EndPoint.java
@@ -1,4 +1,30 @@
 package yaneh.yaneh.utils.Retrofit;
 
-public class EndPoint  {
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface EndPoint  {
+
+//    @FormUrlEncoded
+//    @POST("PhpProject1/index.php")
+//    Call<Void> register(
+//            @Field("name") String name,
+//            @Field("family") String family,
+//            @Field("mobile") String mobile
+//    );
+//
+//    @FormUrlEncoded
+//    @POST("PhpProject1/login.php")
+//    Call<LoginModel> login(
+//            @Field("name") String name,
+//            @Field("family") String family
+//    );
+//
+//    @FormUrlEncoded
+//    @POST("PhpProject1/orders.php")
+//    Call<List<OrdersModel>> orders(
+//            @Field("token") String token
+//    );
 }
