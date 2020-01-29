44e134223fb17ac40a14a7946399c534893aae8e
Maryam <Sedaghati85@gmail.com>
2020-01-29 23:29:04 +0330

    define function of MVP in presenter

--- git-basics/Yaneh/app/src/main/java/yaneh/yaneh/Register/Model.java
+++ git-basics/Yaneh/app/src/main/java/yaneh/yaneh/Register/Model.java
@@ -1,5 +1,10 @@
 package yaneh.yaneh.Register;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import yaneh.yaneh.utils.Gen;
+
 public class Model implements Contract.Model {
     Presenter presenter;
 
@@ -14,7 +19,21 @@ public class Model implements Contract.Model {
         String family = User.getFamily().toString();
         String mobile = User.getMobile().toString();
 
+        Gen.endpoint.register(name,family,mobile).enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if(response.code() == 200) {
+                    presenter.get_msg("اطلاعات شما با موفقیت ثبت شد");
+                }else{
+                    presenter.get_msg("لطفا مجدد تلاش کنید");
+                }
+            }
 
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                presenter.get_msg("لطفا مجدد تلاش کنید");
+            }
+        });
 
     }
 }
