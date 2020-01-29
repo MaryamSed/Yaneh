80290f86dcd758ef40349073c1c21331d39d4e0a
Maryam <Sedaghati85@gmail.com>
2020-01-29 13:18:04 +0330

    create Identity class for name,family,mobile

--- git-basics/Yaneh/app/src/main/java/yaneh/yaneh/Register/RegisterActivity.java
+++ git-basics/Yaneh/app/src/main/java/yaneh/yaneh/Register/RegisterActivity.java
@@ -2,14 +2,31 @@ package yaneh.yaneh.Register;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 import yaneh.yaneh.R;
-
+@EActivity(R.layout.activity_register)
 public class RegisterActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+    @ViewById
+    EditText name , family , mobile;
+    @ViewById
+    Button register;
+
+    @AfterViews
+    void init(){
+
     }
+
+    @Click
+    void register(){
+
+    }
+
 }
