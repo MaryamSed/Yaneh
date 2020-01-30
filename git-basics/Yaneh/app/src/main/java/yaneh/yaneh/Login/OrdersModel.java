21216d4116926df60f12553f4413abb8327a72c7
Maryam <Sedaghati85@gmail.com>
2020-01-30 23:29:27 +0330

    set OrdesModel

---
+++ git-basics/Yaneh/app/src/main/java/yaneh/yaneh/Login/OrdersModel.java
@@ -0,0 +1,43 @@
+
+package yaneh.yaneh.Login;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class OrdersModel {
+
+    @SerializedName("mobile")
+    @Expose
+    private String mobile;
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("token")
+    @Expose
+    private String token;
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+}
