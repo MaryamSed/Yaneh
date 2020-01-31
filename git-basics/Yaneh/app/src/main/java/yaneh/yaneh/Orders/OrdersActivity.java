517ad0f8b32d0fb92a5caaaca8bd625ae4821088
Maryam <Sedaghati85@gmail.com>
2020-02-01 01:15:48 +0330

    set MVP cyrcle im Order

--- git-basics/Yaneh/app/src/main/java/yaneh/yaneh/Orders/OrdersActivity.java
+++ git-basics/Yaneh/app/src/main/java/yaneh/yaneh/Orders/OrdersActivity.java
@@ -3,13 +3,20 @@ package yaneh.yaneh.Orders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
 import yaneh.yaneh.R;
-
+@EActivity(R.layout.activity_orders)
 public class OrdersActivity extends AppCompatActivity {
+    Contract.Presenter presenter = (Contract.Presenter) new Presenter();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_orders);
+    @AfterViews
+    void init(){
+        presenter.attachview((Contract.View) this);
     }
+
+
+
 }
