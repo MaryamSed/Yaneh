5b1ef65a8d54ff4ffbc730326c7eab4b35d51bb0
Maryam <Sedaghati85@gmail.com>
2020-01-31 12:24:13 +0330

    create Orders folder and OrdersActivity

---
+++ git-basics/Yaneh/app/src/main/java/yaneh/yaneh/Orders/OrdersActivity.java
@@ -0,0 +1,15 @@
+package yaneh.yaneh.Orders;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import yaneh.yaneh.R;
+
+public class OrdersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orders);
+    }
+}
